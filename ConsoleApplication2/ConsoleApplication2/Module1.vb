29003d571a6973dfb4a08396d6d6443b11933718
Muhamad Zulfikar <65901202+Muhamadzulfikar@users.noreply.github.com>
2021-11-16 23:15:13 +0800

    Add files via upload

---
+++ ConsoleApplication2/ConsoleApplication2/Module1.vb
@@ -0,0 +1,20 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim mahasiswa As New datamahasiswa
+        mahasiswa.kampus = "Institut Teknologi Batam"
+        mahasiswa.nama = "Muhamad Zulfikar"
+        mahasiswa.nim = 2021018
+        mahasiswa.fakultas = "Teknologi Informasi"
+        mahasiswa.prodi = "Sistem Informasi"
+        Console.WriteLine("Data Mahasiswa")
+        Console.WriteLine("Kampus           : " + mahasiswa.kampus)
+        Console.WriteLine("Nama Mahasiswa   : " + mahasiswa.nama)
+        Console.WriteLine("NIM Mahasiswa    : " + Str(mahasiswa.nim))
+        Console.WriteLine("Fakultas         : " + mahasiswa.fakultas)
+        Console.WriteLine("Prodi            : " + mahasiswa.prodi)
+        Console.ReadLine()
+
+    End Sub
+
+End Module
---
+++ ConsoleApplication2/ConsoleApplication2/datamahasiswa.vb
@@ -0,0 +1,7 @@
+﻿Public Class datamahasiswa
+    Public nama As String
+    Public nim As Integer
+    Public fakultas As String
+    Public prodi As String
+    Public kampus As String
+End Class
